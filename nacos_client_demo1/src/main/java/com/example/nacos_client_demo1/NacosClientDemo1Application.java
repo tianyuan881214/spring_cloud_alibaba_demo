5e7f89762fd73bfe3a10208ea9b822d27568ba68
tianyuan881214 <tianyuan881214@sina.com>
2021-04-08 21:48:32 +0800

    Initial commit

---
+++ nacos_client_demo1/src/main/java/com/example/nacos_client_demo1/NacosClientDemo1Application.java
@@ -0,0 +1,13 @@
+package com.example.nacos_client_demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosClientDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosClientDemo1Application.class, args);
+    }
+
+}
